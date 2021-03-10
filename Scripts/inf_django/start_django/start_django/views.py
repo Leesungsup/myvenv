2c78517a261e179e8f10d322ab5e20fd178797c2
Leesungsup <tmdtjq0116@naver.com>
2021-03-10 22:53:51 +0900

    ?

--- Scripts/inf_django/start_django/start_django/views.py
+++ Scripts/inf_django/start_django/start_django/views.py
@@ -12,4 +12,5 @@ def welcome(request):
     return HttpResponse(html)
 
 def template_test(request):
+    #기본템플릿 폴 1. admin 앱 2. 각 앱의 폴더에 있는 template폴더 3. 우리가 설정한폴더
     return render(request,'test.html')
