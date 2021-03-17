97253ced5c09f3c7d57d15d76173bb7752fc8d03
Leesungsup <tmdtjq0116@naver.com>
2021-03-17 21:48:49 +0900

    CRUD 중 Update, Delete

--- Scripts/inf_django/start_django/bookmark/views.py
+++ Scripts/inf_django/start_django/bookmark/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Bookmark
@@ -15,3 +15,11 @@ class BookmarkCreateView(CreateView):
     template_name_suffix='_create'
 class BookmarkDetailView(DetailView):
     model=Bookmark
+class BookmarkUpdateView(UpdateView):
+    model=Bookmark
+    fields=['site_name','url']
+    template_name_suffix='_update'
+class BookmarkDeleteView(DeleteView):
+    model=Bookmark
+    success_url=reverse_lazy('list')
+    template_name_suffix='_delete'
