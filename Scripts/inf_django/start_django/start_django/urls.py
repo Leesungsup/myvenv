ec4233b252d50d8c974c4682a1ff088bfe06140c
Leesungsup <tmdtjq0116@naver.com>
2021-03-30 22:11:34 +0900

    로그인 html(기능구현X)

--- Scripts/inf_django/start_django/start_django/urls.py
+++ Scripts/inf_django/start_django/start_django/urls.py
@@ -21,6 +21,7 @@ from .views import index,welcome,template_test
 urlpatterns = [
 #1차->2차->3차
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
     path('welcome/', welcome),
     path('test/',template_test),
     path('bookmark/',include('bookmark.urls')),
