2799f796454732052c45b16246de165b1f4a73f8
Leesungsup <tmdtjq0116@naver.com>
2021-04-02 18:29:37 +0900

    Login으로 사용제한하기 (실패)

--- Scripts/inf_django/start_django/photo/views.py
+++ Scripts/inf_django/start_django/photo/views.py
@@ -3,12 +3,15 @@ from .models import Photo
 from django.views.generic.edit import CreateView,DeleteView,UpdateView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+@login_required
 def photo_list(request):
     #보여줄 사진 데이터
     photos=Photo.objects.all()
     return render(request,'photo/list.html',{'photos':photos})
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin,CreateView):
     model=Photo
     fields=['photo','text']#작성자,작성시간
     template_name='photo/upload.html'
@@ -20,11 +23,11 @@ class PhotoUploadView(CreateView):
             return redirect('..')
         else:
             return self.render_to_response({'form':form})
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin,DeleteView):
     model=Photo
     success_url=reverse_lazy('photo_list')
     template_name='photo/delete.html'
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin,UpdateView):
     model=Photo
     fields=['photo','text']
     template_name='photo/update.html'
