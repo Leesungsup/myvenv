2bcc4408f377f1747dd6b0ea3e7e0632b190d440
Leesungsup <tmdtjq0116@naver.com>
2021-04-09 16:33:28 +0900

    detail

--- Scripts/inf_django/start_django/onlineshop/models.py
+++ Scripts/inf_django/start_django/onlineshop/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     slug =models.SlugField(max_length=200,db_index=True,unique=True,allow_unicode=True)
     class Meta:
         ordering=['name']
-        verbose_name='categoty'
+        verbose_name='category'
         verbose_name_plural='categories'
     def __str__(self):
         return self.name
