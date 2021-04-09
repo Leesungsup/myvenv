fbe5d11aed41d2d2a246c6ef8920e663f6b6166a
Leesungsup <tmdtjq0116@naver.com>
2021-04-09 16:47:47 +0900

    list 동작(makemigrations, migrate 기억)

--- Scripts/inf_django/start_django/onlineshop/views.py
+++ Scripts/inf_django/start_django/onlineshop/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,get_object_or_404
 # Create your views here.
 from .models import *
 
-def product_in_category(request,category_slug=None):
+def product_in_category(request, category_slug=None):
     current_category=None
     categories=Category.objects.all()
     products=Product.objects.filter(available_display=True)
@@ -12,6 +12,6 @@ def product_in_category(request,category_slug=None):
         products=products.filter(category=current_category)
     return render(request,'onlineshop/list.html',{'current_category':current_category,'categories':categories,'products':products})
 
-def product_detail(request,id,product_slug=None):
+def product_detail(request, id, product_slug=None):
     product=get_object_or_404(Product,id=id,slug=product_slug)
     return render(request,'onlineshop/detail.html',{'product':product})
