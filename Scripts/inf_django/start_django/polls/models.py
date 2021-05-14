48701ca98ae886598c0ff0b8283e1903b8a6755b
Leesungsup <tmdtjq0116@naver.com>
2021-05-14 21:10:43 +0900

    urls

--- Scripts/inf_django/start_django/polls/models.py
+++ Scripts/inf_django/start_django/polls/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 class Question(models.Model):
-    questiontext=models.TextField();
+    question_text=models.TextField();
     date=models.DateTimeField('date published')
+     def __str__(self):  # __unicode__ on Python 2
+        return self.question_text
+
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
+    def __self__(self):
+        retunr self.choice_text
 
 # Create your models here.
---
+++ Scripts/inf_django/start_django/polls/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+urlpatterns=[
+path('',photo_list,name='photo_list'),
+]
