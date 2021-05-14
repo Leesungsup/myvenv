18e4768c2743e83e1389764e809e7e94e1be9132
Leesungsup <tmdtjq0116@naver.com>
2021-05-14 22:28:45 +0900

    views

--- Scripts/inf_django/start_django/polls/models.py
+++ Scripts/inf_django/start_django/polls/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 class Question(models.Model):
-    question_text=models.TextField();
+    question_text=models.TextField()
     date=models.DateTimeField('date published')
-     def __str__(self):  # __unicode__ on Python 2
+    def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
-    def __self__(self):
-        retunr self.choice_text
+    def __str__(self):
+        return self.choice_text
 
 # Create your models here.
