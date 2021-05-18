47183564ef43ef71ec59dc3550a0cf2c1d9e3a6e
Leesungsup <tmdtjq0116@naver.com>
2021-05-18 18:07:34 +0900

    quiz urls

--- Scripts/inf_django/start_django/myapi/quiz/views.py
+++ Scripts/inf_django/start_django/myapi/quiz/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.responce import Response
+from rest_framework.decorates import api_view
+from .models import Quiz
+from .serializers import QuizSerializer
 # Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("Hello world!")
