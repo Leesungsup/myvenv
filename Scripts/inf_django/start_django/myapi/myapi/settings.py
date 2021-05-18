7f95e2709f97020c52650bd3116c0c12cc370608
Leesungsup <tmdtjq0116@naver.com>
2021-05-18 16:58:33 +0900

    myapi setting time

--- Scripts/inf_django/start_django/myapi/myapi/settings.py
+++ Scripts/inf_django/start_django/myapi/myapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(s(-i%+q+xxy_$gwag5pt9=ku0@rm8)_(%a$byp$iael(_v2%h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [+]
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
